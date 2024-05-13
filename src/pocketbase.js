34496e1ef0ca8d52a942a318c03445d871a33f93
u1978882 <126769186+u1978882@users.noreply.github.com>
2024-05-14 00:54:25 +0200

    ja esta

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,17 +1,2 @@
 import PocketBase from 'pocketbase';
-import { writable } from 'svelte/store';
-
-import { browser, building, dev, version } from '$app/environment';
-
-export let pb
-
-if (dev) pb = new PocketBase('http://192.168.1.41:8090');
-if (building) pb = new PocketBase(window.location.hostname + '8080'); 
-else pb = new PocketBase('http://192.168.1.41:8090');
-
-export const currentUser = writable(pb.authStore.model);
-
-pb.authStore.onChange((auth) => {
-    console.log('authStore changed', auth);
-    currentUser.set(pb.authStore.model);
-});
+export let pb = new PocketBase('http://docktopus.duckdns.org:8090');
